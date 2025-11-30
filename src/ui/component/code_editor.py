56aebd6c3fbe3bc6bcdc7937b1415cba200933e3
Sheng Tai <shengtai25@outlook.com>
2025-11-30 18:43:30 +0200

    convert tab to space and shift tab to indent backwards

--- src/ui/component/code_editor.py
+++ src/ui/component/code_editor.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QPlainTextEdit
-from PySide6.QtGui import QFont
+from PySide6.QtGui import QFont, QKeyEvent
+from PySide6.QtCore import Qt
 
 
 class CodeEditor(QPlainTextEdit):
@@ -14,6 +15,30 @@ class CodeEditor(QPlainTextEdit):
         self.setPlaceholderText("MicroPython")
 
         self.setTabStopDistance(4 * self.fontMetrics().horizontalAdvance(' '))
+
+    def keyPressEvent(self, event: QKeyEvent):
+        """重写键盘事件，将Tab键转换为4个空格"""
+        if event.key() == Qt.Key.Key_Tab:
+            # 插入4个空格而不是Tab字符
+            cursor = self.textCursor()
+            cursor.insertText("    ")  # 4个空格
+            return
+        elif event.key() == Qt.Key.Key_Backtab:
+            # Shift+Tab 删除前面的空格（如果有的话）
+            cursor = self.textCursor()
+            cursor.movePosition(cursor.MoveOperation.StartOfLine, cursor.MoveMode.KeepAnchor)
+            selected_text = cursor.selectedText()
+            if selected_text.endswith("    "):
+                # 删除最后4个空格
+                cursor.removeSelectedText()
+                cursor.insertText(selected_text[:-4])
+                return
+            else:
+                # 恢复选择
+                cursor = self.textCursor()
+
+        # 其他按键使用默认处理
+        super().keyPressEvent(event)
 
     def get_code(self) -> str:
         return self.toPlainText()
